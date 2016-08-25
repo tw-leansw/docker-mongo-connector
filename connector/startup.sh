632c38f53b92d8f11089b0677f5ffa280e58040c
YongliuLi <ylli@thoughtworks.com>
2016-08-25 17:14:16 +0800

    [#0] open verbose mode

--- connector/startup.sh
+++ connector/startup.sh
@@ -27,5 +27,5 @@ do
   fi
 done
 
-mongo-connector --auto-commit-interval=0 -n ${NAME_SPACES} -e ${EXCLUDE_FIELDS} --oplog-ts=/data/oplog.ts -m ${mongo}:${mongoport} -t ${elasticsearch}:${elasticport} -d elastic2_doc_manager --stdout
+mongo-connector --auto-commit-interval=0 -n ${NAME_SPACES} -e ${EXCLUDE_FIELDS} --verbose --oplog-ts=/data/oplog.ts -m ${mongo}:${mongoport} -t ${elasticsearch}:${elasticport} -d elastic2_doc_manager --stdout
 
